745c223f500b0d4887d3f7cc354a962f77b3c3ce
Chane <orozcoveralauramelissa@gmail,com>
2021-09-04 19:45:08 -0500

    corrected things in mainLayout

--- app/src/main/java/com/example/semana3/MainActivity.java
+++ app/src/main/java/com/example/semana3/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.semana3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonConfig, buttonContinuar;
+    private EditText textNombre;
+    private ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
